4d8326a55bce3df6a23badbe12f9a05e917eb2d9
vyckPFH <victoria.c.oliveira08@gmail.com>
2026-06-17 00:54:08 -0300

    roxo

--- script.js
+++ script.js
@@ -44,3 +44,24 @@ botao.addEventListener("click", function () {
     input.value = "";
 });
 
+// Contador - informa quantidade de tarefas pendentes
+function contarPendentes() {
+
+    const tasks = document.querySelectorAll("#taskList li");
+
+    let pendentes = 0;
+
+    tasks.forEach(function (task) {
+        if (!task.classList.contains("concluida")) {
+            pendentes++;
+        }
+    });
+
+    return pendentes;
+}
+
+const pendentesBtn = document.getElementById("pendestes");
+pendentesBtn.addEventListener("click", function () {
+    const pendentes = contarPendentes();
+    alert(`Você possui ${pendentes} tarefas pendentes.`);
+});
